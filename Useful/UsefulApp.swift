1ab72f48216f91cb76074965133a70692f58c0b0
Andrew Gibiansky <agibiansky@apple.com>
2022-12-31 09:21:34 -0800

    Initial Commit

---
+++ Useful/UsefulApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsefulApp.swift
+//  Useful
+//
+//  Created by Andrew Gibiansky on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct UsefulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
